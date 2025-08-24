2f8f676b62a8ceef222f9b2380bbbd07aa6400f0
Bikram <samantaraybikram@gmail.com>
2025-08-24 12:51:03 +0530

    Az updated & AMI updated

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "my_subnet" {
   vpc_id                  = aws_vpc.k8s_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+  availability_zone       = "ap-south-1a"
 
   tags = {
     Name = "my-subnet"
@@ -104,7 +104,7 @@ resource "aws_security_group" "my_sg" {
 # EC2 Instance
 # -----------------------------
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (update for your region)
+  ami           = "ami-0861f4e788f5069dd" # Amazon Linux 2 AMI (update for your region)
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.my_subnet.id
   vpc_security_group_ids = [aws_security_group.my_sg.id]
